680e5272993e8600f1df58569f97bfdba9ad0378
Alpacorp <alejandro.palacios88@gmail.com>
2020-12-19 16:17:10 -0500

    [UPDATE] create endpoints CRUD to users, products

--- index.js
+++ index.js
@@ -7,17 +7,9 @@ const { sequelize } = require("./db_connect");
 
 server.use(bodyparser.json());
 
-server.get('/orders', (req, res) => {
-    sequelize.query('SELECT resto_orders.*, resto_products.*, resto_method_paid_order.* FROM resto_orders INNER JOIN resto_method_paid_order, resto_products', {
-            type: sequelize.QueryTypes.SELECT
-        })
-        .then((orders) => {
-            res.json(orders);
-        })
-        .catch(err => {
-            res.json({ error: err });
-        })
-});
+//ORDERS
+
+//Create
 
 server.post('/orders', (req, res) => {
     const { total_order, status_order, method_paid_order, id_product, id_user } = req.body;
@@ -32,13 +24,82 @@ server.post('/orders', (req, res) => {
         })
 });
 
-server.get('/menu', (req, res) => {
-    sequelize.query('SELECT * FROM resto_products', {
+//Get
+
+server.get('/orders', (req, res) => {
+    sequelize.query('SELECT resto_orders.*, resto_products.*, resto_method_paid_order.* FROM resto_orders INNER JOIN resto_method_paid_order, resto_products', {
             type: sequelize.QueryTypes.SELECT
         })
-        .then((menu) => {
-            res.json({ menu });
+        .then((orders) => {
+            res.json(orders);
         })
+        .catch(err => {
+            res.json({ error: err });
+        })
+});
+
+//PRODUCTS
+
+//Create
+
+server.post('/products', (req, res)=>{
+    const { name_product, description_product, price_product, available_q_product} = req. body;
+    sequelize.query('INSERT INTO resto_products VALUES(null,?,?,?,?,null)', {
+        replacements: [name_product, description_product, price_product, available_q_product]
+    })
+    .then((products)=>{
+        res.status(401).json({message: "Product added sucessfull"});
+    })
+});
+
+//Get
+
+server.get('/products', (req, res)=>{
+    sequelize.query('SELECT * FROM resto_products', {
+        type: sequelize.QueryTypes.SELECT
+    })
+    .then((products)=>{
+        res.json(products);
+    })
+})
+
+//Update
+
+server.put('/products/:id', (req, res)=>{
+    const { name_product, description_product, price_product, available_q_product} = req. body;
+    const id = req.params.id;
+    sequelize.query('UPDATE resto_products SET name_product = ?, description_product = ?, price_product = ?, available_q_product = ? WHERE id_product = ?', {
+        replacements: [name_product, description_product, price_product, available_q_product, id]
+    })
+    .then((products)=>{
+        res.json({message: "product update sucessfully"});
+    });
+});
+
+//Delete
+
+server.delete('/products/:id', (req, res)=>{
+    const id = req.params.id;
+    sequelize.query('DELETE FROM resto_products WHERE id_product = ?',{
+        replacements: [id]
+    })
+    .then((product)=>{
+        res.json({message: "product deleted sucessfully"});
+    });
+});
+
+//USERS
+
+//Create
+
+server.post('/users', (req, res)=>{
+    const {name_user, email_user, phone_user, adress_user, pass_user} = req.body;
+    sequelize.query('INSERT INTO resto_users VALUES(null,?,?,?,?,?,null,DEFAULT)', {
+        replacements: [name_user, email_user, phone_user, adress_user, pass_user]
+    })
+    .then((users)=>{
+        res.status(401).json({message: "user created sucessfully"})
+    })
 })
 
 server.listen(port, () => {
