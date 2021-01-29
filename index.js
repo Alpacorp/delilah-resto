dd638d89c38fe89b566aa3669ce4c2202e0018d4
Alejandro Palacios Arévalo <alejandro.palacios88@gmail.com>
2021-01-28 20:01:10 -0500

    Update port set

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const bodyparser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const signature = "hjtahsujlslppebIU";
