6d9fc9eb168d5de585483ae7e6a5bde0e70f1db0
Alejandro Palacios Arévalo <alejandro.palacios88@gmail.com>
2021-01-28 19:04:48 -0500

    Update dc connection

--- db_connect.js
+++ db_connect.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('mysql://root@localhost:3306/delila_resto');
+// const sequelize = new Sequelize('mysql://root@localhost:3306/delila_resto');
+const sequelize = new Sequelize('mysql://admin:Estefany2021**..@delilah-resto.csajcgloo6ru.us-east-2.rds.amazonaws.com:3306/delila_resto');
 const mysql2 = require("mysql2");
 
 module.exports = {sequelize};
